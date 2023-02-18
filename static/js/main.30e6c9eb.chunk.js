db1b91941a0e9946bdb83ab6fb31dfd25ca48a8f
Code Sandbox <hello@codesandbox.io>
2023-02-18 09:39:54 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.30e6c9eb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmanikanta-react-one"]=this["webpackJsonpmanikanta-react-one"]||[]).push([[0],{12:function(e,t,n){},14:function(e,t,n){"use strict";n.r(t);var c=n(1),a=n(3),o=(n(12),n(0));function r(){return Object(o.jsxs)("div",{className:"App",children:[Object(o.jsx)("h1",{children:"Hello CodeSandbox"}),Object(o.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var i=document.getElementById("root");Object(a.createRoot)(i).render(Object(o.jsx)(c.StrictMode,{children:Object(o.jsx)(r,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.30e6c9eb.chunk.js.map
